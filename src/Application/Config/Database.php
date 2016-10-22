2b0df2fccc3cafead70676d9f992050fc683c9cf
root <enzovaughan@gmail.com>
2016-10-22 17:58:16 -0200

    Senha mysql

--- src/Application/Config/Database.php
+++ src/Application/Config/Database.php
@@ -22,7 +22,7 @@ class Database
         "server" => "mysql.hostinger.com.br",
         "database" => "u694535801_ppf",
         "user" => "u694535801_root",
-        "password" => "123456;;",
+        "password" => "Bn5hLB1UxU",
         "options" => []
     ];
 
